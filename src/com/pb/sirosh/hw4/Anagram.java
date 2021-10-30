faccd487ec152412ddf20f7422b18684cebb339f
Sirosh Tatjana <Tatjana.Sirosh@privatbank.ua>
2021-10-30 20:41:51 +0300

    first commit

--- src/com/pb/sirosh/hw4/Anagram.java
+++ src/com/pb/sirosh/hw4/Anagram.java
@@ -1,29 +1,58 @@
 package com.pb.sirosh.hw4;
-//package string;
-
-
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Anagram {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите строку: ");
-        String in = scan.nextLine();
-        System.out.println(in);
-        print (in);
-//        if (args.length > 0)  // если через консоль были введены аргументы
-//            System.out.println(args[0]);  //то вывести на консоль первый элемент из массива
+        System.out.println("Введите первую строку: ");
+        String str1 = scan.nextLine();
+        System.out.println("Введите вторую строку: ");
+        String str2 = scan.nextLine();
+        if (counter(str1, str2) == false) {
+            System.out.print("Введенные строки не являются анаграммой" + "\n");
+        }
+        else {
+            System.out.print("Введенные строки являются анаграммой" + "\n");
+        }
+    }
+
+    public static boolean counter (String str1, String str2) {
+        //System.out.print("Введенная строка1: " + str1 + "\n"); // для отладки программы
+        //System.out.print("Введенная строка2: " + str2 + "\n"); // для отладки программы
+        if (str1.length() != str2.length()) {
+            //System.out.print("Введенные строки не являются анаграммой" + "\n");
+            return false;
+        }
+
+        char[] str1_array = str1.toCharArray();
+        char[] str2_array = str2.toCharArray();
+        Arrays.sort(str1_array);
+        Arrays.sort(str2_array);
+        String sorted_str1 = new String(str1_array);
+        String sorted_str2 = new String(str2_array);
+        Boolean result = Arrays.equals(str1_array, str2_array);
+       // System.out.println(str1_array); // для отладки программы
+       // System.out.println(str2_array); // для отладки программы
+       // System.out.println(result); // для отладки программы
+
+        return result;
+    }
+
+
+
+
+//    public static void change(String str1, String str2) {
+//        System.out.print("Введенная строка: " + str1); // для отладки программы
+//        System.out.print("Введенная строка: " + str2); // для отладки программы
 //
-//        else {  //иначе —
-//            //S14_MethodsMainOverload obj = new S14_MethodsMainOverload(); //создать объект
-//            main(); // и использовать обычный метод с названием main()
-//        }
-    }
-
-    public static void print (String in) {  // это обычный метод с названием main()
-        System.out.println(in);
-
-    }
+//
+//
+//        //выводим результат
+//        System.out.print("Преобразованный массив: " + "\n");
+//        System.out.println(chArray);
+//    }
 }
 
